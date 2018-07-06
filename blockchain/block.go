ca2bdd5d4028550a5a596bf0bb63c7059a24a245
dongfangzhou <dongfangzhou@58ganji.com>
2018-07-06 16:19:07 +0800

    第一章完成

---
+++ blockchain/block.go
@@ -0,0 +1,38 @@
+package blockchain
+
+import (
+	"strconv"
+	"bytes"
+	"crypto/sha256"
+	"time"
+)
+
+/**
+	区块
+ */
+type Block struct {
+	Timestamp int64 //时间戳
+	PreBlockHash []byte	//上一个区块哈希值
+	Hash []byte	//当前区块哈希值
+	Data []byte	//本区块信息
+}
+
+/**
+	区块-计算自身哈希值
+ */
+func (b *Block)setHash(){
+	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))//获取当前时间戳
+	headers := bytes.Join([][]byte{b.PreBlockHash, b.Data, timestamp}, []byte{})//拼接上区块哈希、本区块数据、时间戳，分隔符为空
+	hash := sha256.Sum256(headers)//生成哈希
+	b.Hash = hash[:]//给bean的hash赋值 byte[low_index:high_index] 数组切片，可以low和high省略
+
+}
+/**
+	创建区块
+ */
+func NewBlock(data string, prevBlockHash []byte) *Block {
+	timestamp := time.Now().Unix()
+	block := &Block{timestamp, prevBlockHash, []byte{}, []byte(data)}
+	block.setHash()
+	return block
+}
--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"./blockchain"
+)
 
+func main() {
+	bc := blockchain.CreateBlockChain()
+	bc.AddBlock("第1个区块")
+	bc.AddBlock("第2个区块")
+	bc.AddBlock("第3个区块")
+
+	for _, block := range bc.Blocks {
+		fmt.Printf("Data：%s\n",block.Data)
+		fmt.Println("Timestamp：",block.Timestamp)
+		fmt.Printf("Hash：%x\n",block.Hash)
+		fmt.Printf("PreBlockHash：%x\n\n",block.PreBlockHash)
+
+	}
 }
+
+
+
